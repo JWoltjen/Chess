d7b602cccbfb5f6f4c2e57fc07e8e3322980f42a
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-21 15:18:28 -0500

    add mouse event for clicking on tiles

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -16,37 +16,35 @@ pieces.push({image: "images/Chess_pdt60.png", x: i, y: 6 })
 for(let i = 0; i < 8; i++){
 pieces.push({image: "images/Chess_plt60.png", x: i, y: 1 })
 }
-
+//rooks
 pieces.push({ image: "images/Chess_rdt60.png", x:0, y: 7})
 pieces.push({ image: "images/Chess_rdt60.png", x:7, y: 7})
 pieces.push({ image: "images/Chess_rlt60.png", x:0, y: 0})
 pieces.push({ image: "images/Chess_rlt60.png", x:7, y: 0})
 
+//bishops
 pieces.push({ image: "images/Chess_bdt60.png", x:2, y: 7})
 pieces.push({ image: "images/Chess_bdt60.png", x:5, y: 7})
 pieces.push({ image: "images/Chess_blt60.png", x:5, y: 0})
 pieces.push({ image: "images/Chess_blt60.png", x:2, y: 0})
 
+//knights
 pieces.push({ image: "images/Chess_ndt60.png", x:1, y: 7})
 pieces.push({ image: "images/Chess_ndt60.png", x:6, y: 7})
 pieces.push({ image: "images/Chess_nlt60.png", x:1, y: 0})
 pieces.push({ image: "images/Chess_nlt60.png", x:6, y: 0})
 
+//kings
 pieces.push({ image: "images/Chess_kdt60.png", x:4, y: 7})
 pieces.push({ image: "images/Chess_klt60.png", x:4, y: 0})
 
+//queens
 pieces.push({ image: "images/Chess_qdt60.png", x:3, y: 7})
 pieces.push({ image: "images/Chess_qlt60.png", x:3, y: 0})
 
-
-
-
-
-
-
-
-
-
+function grabPiece(e: React.MouseEvent) {
+    console.log(e.target)
+}
 export default function Chessboard() {
     let board = []; 
 
@@ -59,8 +57,8 @@ export default function Chessboard() {
                     image = p.image; 
                 }
             })
-            board.push(<Tile image={image} number={number}/>)
+            board.push(<Tile key={`${j},${i}`} image={image} number={number}/>)
            }   
         }
-    return <div id="chessboard">{board}</div>
+    return <div onMouseDown={e => grabPiece(e)} id="chessboard">{board}</div>
     }
