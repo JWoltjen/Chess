35870b70b23a6a8d5123221d64f1b6fb2ef2ad2e
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-21 14:05:45 -0500

    render pawns on every tile

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -11,7 +11,7 @@ export default function Chessboard() {
        for(let i=0; i<horizontalAxis.length; i++){
            const number = j + i + 2; 
 
-            board.push(<Tile number={number}/>)
+            board.push(<Tile image="images/Chess_pdt60.png" number={number}/>)
            }   
         }
     return <div id="chessboard">{board}</div>
