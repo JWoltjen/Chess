e24d058988446e3c00467c4e6711aa807c441def
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-21 17:08:07 -0500

    mouse grabs and drags active piece

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -42,10 +42,28 @@ pieces.push({ image: "images/Chess_klt60.png", x:4, y: 0})
 pieces.push({ image: "images/Chess_qdt60.png", x:3, y: 7})
 pieces.push({ image: "images/Chess_qlt60.png", x:3, y: 0})
 
+let activePiece: HTMLElement | null = null; 
+
 function grabPiece(e: React.MouseEvent) {
     const element = e.target as HTMLElement; 
     if(element.classList.contains("chess-piece")){
-        console.log(e)
+        const x = e.clientX - 50; 
+        const y = e.clientY - 50; 
+        element.style.position = "absolute";
+        element.style.left = `${x}px`; 
+        element.style.top = `${y}px`; 
+
+        activePiece = element; 
+    }
+}
+function movePiece(e: React.MouseEvent) {
+    const element = e.target as HTMLElement; 
+    if(activePiece){
+        const x = e.clientX - 50; 
+        const y = e.clientY - 50; 
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`; 
+        activePiece.style.top = `${y}px`
     }
 }
 export default function Chessboard() {
@@ -63,5 +81,5 @@ export default function Chessboard() {
             board.push(<Tile key={`${j},${i}`} image={image} number={number}/>)
            }   
         }
-    return <div onMouseDown={e => grabPiece(e)} id="chessboard">{board}</div>
+    return <div onMouseMove={(e) => movePiece(e)} onMouseDown={e => grabPiece(e)} id="chessboard">{board}</div>
     }
