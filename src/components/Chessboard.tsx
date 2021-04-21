9537d4bcef3b039bb0028f633e62649b552ce139
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-21 13:46:32 -0500

    add conditional black white logic to tile component

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,25 +1,18 @@
 import './Chessboard.css'
-import './Tile'
+import Tile from './Tile'
 const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8']
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 
 export default function Chessboard() {
     let board = []; 
-    for(let j = verticalAxis.length-1; j >= 0; j--)
+
+    for(let j = verticalAxis.length-1; j >= 0; j--){
        for(let i=0; i<horizontalAxis.length; i++){
            const number = j + i + 2; 
 
-           if(number%2 === 0){
-               board.push(<Tile/>)
-           } else {
-                board.push(<Tile/>)
-           }
-        }      
-
-    return (
-        <div id="chessboard">
-            {board}
-        </div>
-    )
-}
+            board.push(<Tile number={number}/>)
+           }   
+        }
+    return <div id="chessboard">{board}</div>
+    }
