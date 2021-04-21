9b5410e2dee85ac2159083e188ee6d80930ccf0a
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-21 14:12:42 -0500

    render single pawn on starting location

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -3,15 +3,26 @@ import Tile from './Tile'
 const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8']
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-
+interface Piece {
+    image: string
+    x: number
+    y: number
+}
+const pieces: Piece[] = []
+pieces.push({image: "images/Chess_pdt60.png", x: 0, y: 1 })
 export default function Chessboard() {
     let board = []; 
 
     for(let j = verticalAxis.length-1; j >= 0; j--){
        for(let i=0; i<horizontalAxis.length; i++){
            const number = j + i + 2; 
-
-            board.push(<Tile image="images/Chess_pdt60.png" number={number}/>)
+           let image = undefined; 
+            pieces.forEach(p => {
+                if(p.x === i && p.y === j){
+                    image = p.image; 
+                }
+            })
+            board.push(<Tile image={image} number={number}/>)
            }   
         }
     return <div id="chessboard">{board}</div>
